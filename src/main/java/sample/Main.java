bee80fbae576fb7b6ef86406da46ac4ad7a1ae92
Matthias Wetzlmayer <mwetzl@gmail.com>
2020-02-28 10:23:55 +0100

    Server finished

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,15 +1,21 @@
 package sample;
 
+import at.orderlibrary.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main extends Application {
+    ReentrantLock lock;
+    int port = 17774;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -20,4 +26,17 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+
+    public static ArrayList<Offer> readAllOffers() {
+        ArrayList<Offer> offers = new ArrayList<>();
+        offers.add(new Offer(1, "Schnitzel deluxe premium", 8.6, Category.FOOD));
+        offers.add(new Offer(2, "Hendl", 5.6, Category.FOOD));
+        offers.add(new Offer(3, "Bier", 4.5, Category.ALCOHOLIC_DRINK));
+        offers.add(new Offer(4, "Apfelsaft", 2.5, Category.NON_ALCOHOLIC_DRINK));
+        offers.add(new Offer(2, "Würstel", 5.6, Category.FOOD));
+
+        return offers;
+    }
+
 }
